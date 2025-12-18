7fae21b40ef1dcf75ebb80091f0dc59959ac05d2
Melek-Ustun <melek.ustun@etu.u-pec.fr>
2025-12-18 20:14:36 +0100

    Affichage et suppression

--- src/ActionBDDImpl.java
+++ src/ActionBDDImpl.java
@@ -1,82 +1,139 @@
 import java.sql.*;
 import java.util.ArrayList;
 import java.sql.*;
+import java.util.Scanner;
 
 
 public class ActionBDDImpl {
     public static void main(String[] args) throws SQLException {
         Connection conn = connectToDatabase();
+        ListeProgrammeurs(conn);
+        affichageProgrammeurByID(conn);
+        supprimerProgrammeur(conn);
     }
 
 
     // Connexion à la base de donnée
-        public static Connection connectToDatabase() {
-            Connection conn = null;
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/Prog_BD",
-                        "melek",
-                        "password"
+    public static Connection connectToDatabase() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Prog_BD",
+                    "melek",
+                    "password"
+            );
+            System.out.println("Connected to database successfully");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    //        // Liste des programmeurs
+    public static ArrayList<Programmeur> ListeProgrammeurs(Connection conn) {
+        ArrayList<Programmeur> liste = new ArrayList<>();
+
+        try {
+            Statement stmnt = conn.createStatement();
+            ResultSet rs = stmnt.executeQuery("SELECT * FROM PROGRAMMEUR");
+
+            while (rs.next()) {
+                Programmeur p = new Programmeur(
+                        rs.getInt("id"),
+                        rs.getString("nom"),
+                        rs.getString("prenom"),
+                        rs.getString("adresse"),
+                        rs.getString("pseudo"),
+                        rs.getString("responsable"),
+                        rs.getString("hobby"),
+                        rs.getInt("annaissance"),
+                        rs.getDouble("salaire"),
+                        rs.getDouble("prime")
                 );
-                System.out.println("Connected to database successfully");
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
+                liste.add(p);
             }
-            return conn;
+            System.out.println("Liste des programmeurs:");
+            System.out.println(liste);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
-//        // Liste des programmeurs
-//        public static ArrayList<Programmeur> ListeProgrammeurs(Connection conn) {
-//            ArrayList<Programmeur> liste = new ArrayList<>();
-//
-//            try {
-//                Statement stmnt = conn.createStatement();
-//                ResultSet rs = stmnt.executeQuery("SELECT * FROM PROGRAMMEUR");
-//
-//                while (rs.next()) {
-//                    Programmeur p = new Programmeur(
-//                            rs.getString(1),
-//                            rs.getString(2),
-//                            rs.getInt(3),
-//                            rs.getDouble(4),
-//                            rs.getDouble(5)
-//                    );
-//                    liste.add(p);
-//
-//                                   }
-//                System.out.println("Liste des programmeurs:");
-//                System.out.println(liste);
-//
-//
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//
-//            return liste;
-//        }
+        return liste;
+    }
 
-//        // Affichage des programmeurs
-//        public static void affichageProgrammeur(Connection conn) {
-//            try {
-//                System.out.println("Affichage des programmeurs:");
-//                Statement stmnt = conn.createStatement();
-//                ResultSet rs = stmnt.executeQuery("SELECT * FROM PROGRAMMEUR");
-//
-//                while (rs.next()) {
-//                    Programmeur p = new Programmeur(
-//                            rs.getString(1),
-//                            rs.getString(2),
-//                            rs.getInt(3),
-//                            rs.getDouble(4),
-//                            rs.getDouble(5)
-//                    );
-//                }
-//
-//            } catch (SQLException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
+    // Affichage des programmeurs
+    public static void affichageProgrammeurByID(Connection conn) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("ID du programmeur à afficher : ");
+            Integer sc = Integer.valueOf(scanner.nextLine());
+
+            PreparedStatement pstmt = conn.prepareStatement(
+                    "SELECT * FROM `programmeur` WHERE id = ? ");
+            pstmt.setInt(1, sc);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Programmeur p = new Programmeur(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getInt(8),
+                        rs.getDouble(9),
+                        rs.getDouble(10)
+                );
+                System.out.println(p.toString());
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Supprimer un programmeur
+    public static void supprimerProgrammeur(Connection conn) {
+        Scanner scanner = new Scanner(System.in);
+        boolean suppressionReussie = false;
+
+        while (!suppressionReussie) {
+            try {
+                System.out.print("ID du programmeur à supprimer : ");
+                Integer id = Integer.valueOf(scanner.nextLine());
+
+                PreparedStatement pstmt = conn.prepareStatement(
+                        "DELETE FROM `programmeur` WHERE id = ?");
+                pstmt.setInt(1, id);
+                int rs = pstmt.executeUpdate();
+
+                if (rs > 0) {
+                    System.out.println("Suppression réussie !");
+                    suppressionReussie = true;
+                } else {
+                    throw new IllegalArgumentException("Supression KO. Saisissez à nouveau l'id :");
+                }
+
+                pstmt.close();
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.out.println("\n");
+            } catch (SQLException e) {
+                System.out.println("Erreur SQL : " + e.getMessage());
+                break;
+            }
+        }
+    }
+
+
+
 //
 //        // Affichage des programmeurs dont le nom est différent de Simpson
 //        public static void ProgrammeursExceptSimpson(Connection conn) {
@@ -100,5 +157,6 @@ public class ActionBDDImpl {
 //
 //    }
 
-}
+    }
+
 
--- src/Programmeur.java
+++ src/Programmeur.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class Programmeur {
+    Integer id;
+    String nom;
+    String prenom;
+    String adresse;
+    String responsable;
+    String hobby;
+    Integer annaissance;
+    double salaire;
+    double prime;
+    String pseudo;
+
+
+    public Programmeur(Integer id,String nom, String prenom,String adresse, String pseudo, String responsable, String hobby, Integer annaissance, double salaire, double prime) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.pseudo = nom + " " + prenom;
+        this.responsable = responsable;
+        this.hobby = hobby;
+        this.annaissance = annaissance;
+        this.salaire = salaire;
+        this.prime = prime;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return id + " "+  nom + " " + prenom + " " + annaissance + " " + salaire + " " + prime +  " " + pseudo;
+    }
+
 }
+
